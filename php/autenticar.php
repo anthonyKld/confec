a21d29ccc6cbceae3f819da09fd1cc7ae6abc9a1
Anthony Kildery <90473131+anthonyKld@users.noreply.github.com>
2024-08-09 08:06:25 -0400

    Teste 03 session

--- php/autenticar.php
+++ php/autenticar.php
@@ -13,14 +13,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $stmt->bind_param("ss", $email, $senha_criptografada);
     $stmt->execute();
     $result = $stmt->get_result();
+    $dados = $result->fetch_assoc();
 
     if ($result->num_rows > 0) {
         // Autenticação bem-sucedida
         $_SESSION['usuario'] = $email;
-        $_SESSION['nome'] = $email;
-        print_r ($result);
-        //header("Location: ./"); // Redireciona para a página principal após o login
-        //exit(); // Certifique-se de que o script pare de executar após o redirecionamento
+        $_SESSION['nomeUsuario'] = $dados['nome'];
+        header("Location: ./"); // Redireciona para a página principal após o login
+        exit(); // Certifique-se de que o script pare de executar após o redirecionamento
     } else {
         // Autenticação falhou
         $_SESSION['erro'] = "E-mail ou senha incorretos.";
